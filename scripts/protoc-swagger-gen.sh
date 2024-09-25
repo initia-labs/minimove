48d0ff9119e5de99cddc364dcc20bf5376f12bb6
beer-1 <beer-1@codingcats.xyz>
2024-09-25 17:13:56 +0900

    update slinky => connect/v2

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -9,14 +9,15 @@ IBC_V=v8
 INITIA_URL=github.com/initia-labs/initia
 OPINIT_URL=github.com/initia-labs/OPinit
 INDEXER_URL=github.com/initia-labs/kvindexer
-SLINKY_URL=github.com/skip-mev/slinky
+CONNECT_URL=github.com/skip-mev/connect
+CONNECT_V=v2
 
 COSMOS_SDK_VERSION=$(cat ./go.mod | grep "$COSMOS_URL v" | sed -n -e "s/^.* //p")
 IBC_VERSION=$(cat ./go.mod | grep "$IBC_URL/$IBC_V v" | sed -n -e "s/^.* //p")
 INITIA_VERSION=$(cat ./go.mod | grep "$INITIA_URL v" | sed -n -e "s/^.* //p")
 OPINIT_VERSION=$(cat ./go.mod | grep "$OPINIT_URL v" | sed -n -e "s/^.* //p")
 INDEXER_VERSION=$(cat ./go.mod | grep "$INDEXER_URL v" | sed -n -e "s/^.* //p")
-SLINKY_VERSION=$(cat ./go.mod | grep "$SLINKY_URL v" | sed -n -e "s/^.* //p")
+CONNECT_VERSION=$(cat ./go.mod | grep "$CONNECT_URL/$CONNECT_V v" | sed -n -e "s/^.* //p")
 
 mkdir -p ./third_party
 cd third_party
@@ -25,7 +26,7 @@ git clone -b $OPINIT_VERSION https://$OPINIT_URL
 git clone -b $COSMOS_SDK_VERSION https://$COSMOS_URL
 git clone -b $IBC_VERSION https://$IBC_URL
 git clone -b $INDEXER_VERSION https://$INDEXER_URL
-git clone -b $SLINKY_VERSION https://$SLINKY_URL
+git clone -b $CONNECT_VERSION https://$CONNECT_URL
 cd ..
 
 
@@ -38,7 +39,7 @@ proto_dirs=$(find \
   ../third_party/initia/proto \
   ../third_party/opinit/proto \
   ../third_party/kvindexer/proto \
-  ../third_party/slinky/proto \
+  ../third_party/connect/proto \
   -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
