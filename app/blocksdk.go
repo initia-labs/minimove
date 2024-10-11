26d7eebf4abc204d5b38ab3f6cd5720f117bf426
beer-1 <beer-1@codingcats.xyz>
2024-10-11 16:44:07 +0900

    increase system lane gas limit

--- app/blocksdk.go
+++ app/blocksdk.go
@@ -41,7 +41,7 @@ func setupBlockSDK(
 		Logger:          app.Logger(),
 		TxEncoder:       app.txConfig.TxEncoder(),
 		TxDecoder:       app.txConfig.TxDecoder(),
-		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.01"),
+		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.1"),
 		MaxTxs:          1,
 		SignerExtractor: signerExtractor,
 	}, opchildlanes.SystemLaneMatchHandler())
@@ -51,7 +51,7 @@ func setupBlockSDK(
 		Logger:          app.Logger(),
 		TxEncoder:       app.txConfig.TxEncoder(),
 		TxDecoder:       app.txConfig.TxDecoder(),
-		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.09"),
+		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.1"),
 		MaxTxs:          100,
 		SignerExtractor: signerExtractor,
 	}, factory, factory.MatchHandler())
@@ -69,7 +69,7 @@ func setupBlockSDK(
 		Logger:          app.Logger(),
 		TxEncoder:       app.txConfig.TxEncoder(),
 		TxDecoder:       app.txConfig.TxDecoder(),
-		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.8"),
+		MaxBlockSpace:   math.LegacyMustNewDecFromStr("0.7"),
 		MaxTxs:          mempoolMaxTxs,
 		SignerExtractor: signerExtractor,
 	})
