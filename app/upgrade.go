43d744f5b8217c962a529a2d0f4b540f98b8e87e
beer-1 <beer-1@codingcats.xyz>
2024-11-15 17:51:04 +0900

    prepare v0.6.3 upgrade

--- app/upgrade.go
+++ app/upgrade.go
@@ -9,7 +9,7 @@ import (
 	opchildtypes "github.com/initia-labs/OPinit/x/opchild/types"
 )
 
-const upgradeName = "0.6.2"
+const upgradeName = "0.6.3"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *MinitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
@@ -22,11 +22,12 @@ func (app *MinitiaApp) RegisterUpgradeHandlers(cfg module.Configurator) {
 			}
 
 			// set non-zero default values for new params
-			params.HookMaxGas = opchildtypes.DefaultHookMaxGas
-
-			err = app.OPChildKeeper.SetParams(ctx, params)
-			if err != nil {
-				return nil, err
+			if params.HookMaxGas == 0 {
+				params.HookMaxGas = opchildtypes.DefaultHookMaxGas
+				err = app.OPChildKeeper.SetParams(ctx, params)
+				if err != nil {
+					return nil, err
+				}
 			}
 
 			return vm, nil
