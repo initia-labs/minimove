3b68a1b85d84c5823380bb1512c747afa418dd07
beer-1 <beer-1@codingcats.xyz>
2025-04-29 20:45:32 +0900

    increase default query gas limit to 10m

--- cmd/minitiad/config.go
+++ cmd/minitiad/config.go
@@ -41,7 +41,7 @@ func initAppConfig() (string, interface{}) {
 	// In simapp, we set the min gas prices to 0.
 	srvCfg.MinGasPrices = fmt.Sprintf("0%s", types.BaseDenom)
 	srvCfg.Mempool.MaxTxs = 2000
-	srvCfg.QueryGasLimit = 5000000
+	srvCfg.QueryGasLimit = 10_000_000
 	srvCfg.InterBlockCache = false
 
 	// Enable API and unsafe CORS (CORS allowed from any host)
@@ -54,7 +54,7 @@ func initAppConfig() (string, interface{}) {
 	srvCfg.GRPC.Address = "0.0.0.0:9090"
 
 	moveCfg := moveconfig.DefaultMoveConfig()
-	moveCfg.ContractSimulationGasLimit = 5000000
+	moveCfg.ContractSimulationGasLimit = 10_000_000
 
 	initiaappConfig := initiaappConfig{
 		Config:        *srvCfg,
